e9a6692c2ce02b71df451cf80fd651324084155c
Mariella <mariella.colombo@ladyj.eu>
2016-08-01 16:07:05 +0200

    Update scanf.php

--- Manual/Function_reference/Text_processing/scanf.php
+++ Manual/Function_reference/Text_processing/scanf.php
@@ -18,3 +18,24 @@ string(3) "bau"
 string(4) "ciao"
 NULL
 */
+
+/*
+Any whitespace in the format string matches any 
+whitespace in the input string. 
+This means that even a tab \t in the format string 
+can match a single space character in the input string.
+*/
+
+list($a, $b, $c, $d) = sscanf($a, "%s\t%s\t%s\t%s\n");
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+
+/*
+string(4) "maio"
+string(3) "bau"
+string(4) "ciao"
+string(5) "simba"
+*/
