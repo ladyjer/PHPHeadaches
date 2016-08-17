2f710bbbe4de2040eb79790e5661742ac8859628
LadyJ <mariella.colombo@ladyj.eu>
2016-08-17 16:57:46 +0200

    Altre cose strane

--- Manual/Language_Reference/_8_Classes_and_obj/_2__get.php
+++ Manual/Language_Reference/_8_Classes_and_obj/_2__get.php
@@ -13,16 +13,7 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  * @link       http://www.ladyj.eu
  *
- * Consider the following code. What change must be made to the class for the code to work as written?
- *
- * class Magic { protected $v = array("a" => 1, "b" => 2, "c" => 3); public function __get($v) { return $this->v[$v]; } } $m = new Magic(); $m->d[] = 4; echo $m->d[0];
- * Consider the following code. What change must be made to the class for the code to work as written? class Magic { protected $v = array("a" => 1, "b" => 2, "c" => 3); public function __get($v) { return $this->v[$v]; } } $m = new Magic(); $m->d[] = 4; echo $m->d[0];
- * Nothing, this code works just fine.
- * Rewrite __get as: public function &__get($v)
- * E: Make __get method static
- * Add __set method doing $this->v[$var] = $val
- * Rewrite __get as: public function __get(&$v)
- * */
+ **/
 class Magic
 {
 	protected $v = array("a" => 1, "b" => 2, "c" => 3);
