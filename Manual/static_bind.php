183204f904014c39579aca8771c02c4a35e7773e
LadyJ <mariella.colombo@ladyj.eu>
2016-08-20 14:00:16 +0200

    Varie

--- Manual/static_bind.php
+++ Manual/static_bind.php
@@ -5,9 +5,6 @@
  *
  * PHP version 5
  *
- * @category   Joomla_Components
- * @package    Joomla.Administrator
- * @subpackage Com_Arxivar
  * @author     Mariella Colombo <mariella.colombo@ladyj.eu>
  * @copyright  Copyright © 2016 Colombo Mariella
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
